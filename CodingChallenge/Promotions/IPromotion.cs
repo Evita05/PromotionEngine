16e5d59f1ac37081124633db46492c6527b272a6
Evita05 <50552411+Evita05@users.noreply.github.com>
2020-07-02 18:08:57 +0530

    Create IPromotion.cs
    Creating an interface, so that if any methods are common can be placed here. As of now the method setPromotion is there which will be implemented by the classes to do the calculation.

---
+++ CodingChallenge/Promotions/IPromotion.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CodingChallenge;
+using CodingChallenge.Carts;
+
+namespace CodingChallenge.Promotions
+{
+    public interface IPromotion
+    {
+        int setPromotion(Cart cart);
+
+        
+    }
+}
